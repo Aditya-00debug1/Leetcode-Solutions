a73f2cb407b2c79df93330d52bb010c6e3dddf3c
Aditya-00debug1 <adikanha07@gmail.com>
2026-07-28 23:25:12 +0530

    Time: 19 ms (7.13%), Space: 23.8 MB (12.46%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n =nums.size();
+       unordered_map<int,int> hash;
+        for(int x:nums){
+            hash[x]++;
+        }
+        for(int x:nums){
+            if(hash[x] == 1){
+                return x;
+            }
+        }
+        return -1;
+        
+        
+
+    }
+};
